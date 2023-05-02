7ca7141cd1099dc528e5f42d9f5f9253e9310666
tayzhang <tayzhang@tencent.com>
2023-05-02 23:47:54 +0800

    add service code and main function
    Signed-off-by: tayzhang <tayzhang@tencent.com>

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,9 @@
 # use oapi-codegen to generate server and struct code
 # https://github.com/deepmap/oapi-codegen
-oapi-codegen -package api -generate types taylorpetstore.yaml > autogen/taylorpetstore.types.gen.go
-oapi-codegen -package api -generate spec taylorpetstore.yaml > autogen/taylorpetstore.spec.gen.go
-oapi-codegen -package api -generate gin taylorpetstore.yaml > autogen/taylorpetstore.ginserver.gen.go
-oapi-codegen -package api -generate client taylorpetstore.yaml > autogen/taylorpetstore.client.gen.go
+oapi-codegen -package api -generate types taylorpetstore.yaml > api/taylorpetstore.types.gen.go
+oapi-codegen -package api -generate spec taylorpetstore.yaml > api/taylorpetstore.spec.gen.go
+oapi-codegen -package api -generate gin taylorpetstore.yaml > api/taylorpetstore.ginserver.gen.go
+oapi-codegen -package api -generate client taylorpetstore.yaml > api/taylorpetstore.client.gen.go
 
 # use widdershins to generate markdown documentation
 # https://github.com/Mermade/widdershins
---
+++ main.go
@@ -0,0 +1,64 @@
+// This is an example of implementing the Pet Store from the OpenAPI documentation
+// found at:
+// https://github.com/OAI/OpenAPI-Specification/blob/master/examples/v3.0/petstore.yaml
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"taylorzhangyx.github.com/taylorpetstore/api"
+
+	middleware "github.com/deepmap/oapi-codegen/pkg/gin-middleware"
+)
+
+func NewGinPetServer(petStore *api.PetStore, host string, port int) *http.Server {
+	swagger, err := api.GetSwagger()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error loading swagger spec\n: %s", err)
+		os.Exit(1)
+	}
+
+	// Clear out the servers array in the swagger spec, that skips validating
+	// that server names match. We don't know how this thing will be run.
+	swagger.Servers = nil
+
+	// This is how you set up a basic gin router
+	r := gin.Default()
+
+	// Enabling CORS, for more info see: https://github.com/gin-contrib/cors
+	r.Use(cors.Default())
+
+	// Use our validation middleware to check all requests against the
+	// OpenAPI schema.
+	r.Use(middleware.OapiRequestValidator(swagger))
+
+	// We now register our petStore above as the handler for the interface
+	api.RegisterHandlers(r, petStore)
+
+	s := &http.Server{
+		Handler: r,
+		Addr:    fmt.Sprintf("%s:%d", host, port),
+	}
+	return s
+}
+
+func main() {
+	var port = flag.Int("port", 8080, "Port for test HTTP server")
+	var host = flag.String("host", "localhost", "Host for test HTTP server")
+
+	flag.Parse()
+	// Create an instance of our handler which satisfies the generated interface
+	petStore := api.NewPetStore()
+	s := NewGinPetServer(petStore, *host, *port)
+
+	fmt.Printf("Serving on %s:%d\n", *host, *port)
+	// And we serve HTTP until the world ends.
+	log.Fatal(s.ListenAndServe())
+}
